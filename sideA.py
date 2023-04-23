1099622266b6685b3724cb4ee54805efa80988d0
yuval hadar <yuvalhad12@gmail.com>
2023-04-23 13:46:25 +0300

    finished implementing udp encryption

--- sideA.py
+++ sideA.py
@@ -1,5 +1,5 @@
 import struct
-
+import enc
 import aioudp
 import asyncio
 
@@ -19,7 +19,7 @@ Algorithm:
 rest is for the data itself...
 """
 
-file = [bytes([0, 1, 2, 3, 4, 5]) * 21230]
+file = [bytes([1, 2, 3, 4, 5]) * 1]
 my_addr = ("127.0.0.1", 1234)
 
 TYPES = {
@@ -29,6 +29,7 @@ TYPES = {
     "KEEP ALIVE": 4
 }
 
+
 def keep_alive(trans_id):
     return struct.pack('i b b', 6, trans_id, TYPES["KEEP ALIVE"])
 
@@ -37,8 +38,13 @@ def parse_request(payload):
     return struct.unpack('i b b i i i', payload)
 
 
-def get_block_of_file(piece_index, block_offset, block_length):
-    return file[piece_index][block_offset:block_length + block_offset]
+def get_block_of_file(piece_index, block_offset, block_length, public_key):
+    # WHAT DO WE DO WITH PUBLIC KEY
+    block_requested = file[piece_index][block_offset:block_length + block_offset]
+    if not public_key:
+        return block_requested
+
+    return enc.encrypt_using_public(block_requested, public_key)
 
 
 def create_data_msg(data, trans_id, piece_index, block_offset, block_length):
@@ -49,22 +55,22 @@ def create_data_msg(data, trans_id, piece_index, block_offset, block_length):
     # 4 bytes - block offset
     # 4 bytes - block length
     # rest - data
-    return struct.pack('i b b i i i', 4 + 1 + 1 + 4 + 4 + 4 + len(data) + 1, trans_id, TYPES["PIECE"], piece_index, block_offset, block_length) + data
+    return struct.pack('i b b i i i', 4 + 1 + 1 + 4 + 4 + 4 + len(data) + 1, trans_id, TYPES["PIECE"], piece_index,
+                       block_offset, block_length) + data
 
 
-async def parse_init_msg(msg, addr):
+def parse_init_msg(msg, addr):
     ip_table = []
     offset = len(ip_table)
     if addr[0] not in ip_table:
         offset += 1
         ip_table.append(addr[0])
 
-
     if msg == b'bittorrent':
-        return (struct.pack('! b', offset), None)
-    
+        return struct.pack('! b', offset), None
+
     public_pem = msg[10:]
-    return (struct.pack('! b', offset) + public_pem, public_pem)
+    return struct.pack('! b', offset) + public_pem, public_pem
 
 
 async def listen():
@@ -74,27 +80,24 @@ async def listen():
     msg, addr = await conn_with_peer.receive()
 
     resp, public_key = parse_init_msg(msg, addr)
+
     conn_with_peer.send(resp, addr)
-    
+    print("finished handshake with peer")
 
-    sleep_cntr = 1
     while True:
-        if sleep_cntr % 30 == 0:
-            conn_with_peer.send(keep_alive(trans_id))
-            sleep_cntr = 0
-
         msg, addr = await conn_with_peer.receive()
         msg_length, trans_id, msg_type, piece_index, block_offset, block_length = parse_request(msg)
 
-        if msg_type == 1:
-            data = get_block_of_file(piece_index, block_offset, block_length)
+        if msg_type == TYPES["REQUEST"]:
+            data = get_block_of_file(piece_index, block_offset, block_length, public_key)
             block_len = len(data)
             print(f"data being sent: {data}")
             data = create_data_msg(data, trans_id, piece_index, block_offset, block_len)
-            print(f"sent to peer data, piece index: {piece_index}, block offset: {block_offset}, block length: {block_len}")
+            print(
+                f"sent to peer data, piece index: {piece_index}, block offset: {block_offset}, block length: {block_len}")
             conn_with_peer.send(data, addr)
 
         await asyncio.sleep(0.001)
-        sleep_cntr += 0.001
+
 
 asyncio.run(listen())
--- sideB.py
+++ sideB.py
@@ -1,11 +1,12 @@
 import struct
-
+import enc
 import aioudp
 import asyncio
 
 peer_addr = ("127.0.0.1", 1234)
-file = bytearray([0]) * 6 * 21230
-print(f"file length: {len(file)}")
+file = bytearray([0, 0, 0, 0, 0]) * 1
+public_key, private_key = enc.create_key_pairs()
+
 TYPES = {
     "REQUEST": 1,
     "PIECE": 2,
@@ -26,7 +27,12 @@ def create_piece_request(trans_id, piece_index, block_offset, length):
                        length)
 
 
-def build_piece_into_file(data, offset, block_length):
+def build_piece_into_file(data, offset, block_length, public_key=None):
+    # public key value is not used; it's just to understand if torrent is encrypted or not. 
+    if public_key:
+        data = enc.decrypt_using_private(data, private_key)
+        block_length = len(data)
+
     for i in range(block_length):
         file[i + offset] = data[i]
 
@@ -36,46 +42,61 @@ def build_piece_into_file(data, offset, block_length):
         exit(1)
 
 
-async def generate_init_msg(public_pem = None):
+def generate_init_msg(public_pem=None):
     if not public_pem:
         return b'bittorrent'
-    
+
     return b'bittorrent' + public_pem
-    
+
+
+def get_trans_id_from_resp(resp, public_pem=None):
+    if public_pem:
+        assert resp[1:] == public_pem
+        return resp[0]
+
+    return resp[0]
+
+
+def parse_block_message(msg):
+    # 4 bytes - length
+    # 1 bytes - trans id
+    # 1 bytes - msg type
+    # 4 bytes - piece
+    # 4 bytes - block offset
+    # 4 bytes - block length
+    # rest - data
+    return struct.unpack('i b b i i i', msg)
+
 
 async def main():
     BLOCK_SIZE = 0xfff
     block_offset = 0
 
+    if public_key:
+        BLOCK_SIZE = enc.MSG_SIZE_REQUEST_BYTES
+
     remote_conn = await aioudp.open_remote_endpoint(*peer_addr)
-    remote_conn.send(b'bittorrent')
+    remote_conn.send(generate_init_msg(public_key))
+    print("sent init msg")
+
     trans_id = await remote_conn.receive()
-    trans_id = int.from_bytes(trans_id)
+    trans_id = get_trans_id_from_resp(trans_id)
+    print("finished handshake")
 
     while True:
         req = create_piece_request(trans_id, 0, block_offset, BLOCK_SIZE)
         remote_conn.send(req)
+        print("sent a piece request msg")
         msg = await remote_conn.receive()
-
         # msg should be of type:
         # piece, no piece
 
-        # 4 bytes - length
-        # 1 bytes - trans id
-        # 1 bytes - msg type
-        # 4 bytes - piece
-        # 4 bytes - block offset
-        # 4 bytes - block length
-        # rest - data
-
-        if msg[5] == TYPES["KEEP ALIVE"]:
-            continue
-
-        length, trans_id, msg_type, piece_index, recv_offset, block_length = struct.unpack('i b b i i i', msg[:20])
-        print(f"recieved data from peer, piece index: {piece_index}, block offset: {recv_offset}, block length: {block_length}")
+        length, trans_id, msg_type, piece_index, recv_offset, block_length = parse_block_message(msg[:20])
+        print(
+            f"recieved data from peer, piece index: {piece_index}, block offset: {recv_offset}, block length: {block_length}")
 
         if msg_type == TYPES["PIECE"]:
-            build_piece_into_file(msg[20:length + 1], block_offset, block_length)
+            build_piece_into_file(msg[20:length + 1], block_offset, block_length, public_key)
             block_offset += BLOCK_SIZE
 
         await asyncio.sleep(0.001)
